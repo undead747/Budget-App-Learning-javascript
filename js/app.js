bbbb2093883963682684d097d33f020308b9bfb8
undead747 <narutominh747@gmail.com>
2019-07-21 21:15:28 +0700

    validate input

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 class UI {
   constructor() {
     this.budgetFeedback = document.querySelector(".budget-feedback");
-    this.expenseFeedback = document.querySelector(".expense-feedback");
+    this.expenseNameFeedback = document.querySelector(".expenseName-feedback");
+    this.expenseValueFeedback = document.querySelector(".expenseValue-feedback");
     this.budgetForm = document.getElementById("budget-form");
     this.budgetInput = document.getElementById("budget-input");
     this.budgetAmount = document.getElementById("budget-amount");
@@ -15,35 +16,75 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
-    
-    submitBudgetForm(){
-       window.alert("Minh Dep Trai");
+
+  submitBudgetForm() {
+    const budgetInput = this.budgetInput.value;
+
+    if (budgetInput === '' || budgetInput < 0) {
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML = `<p>your budget can't be empty or negative </p>`;
+
+      const self = this;
+
+      setTimeout(function () {
+        self.budgetFeedback.classList.remove("showItem");
+      }, 3000);
     }
-} 
+  }
 
-function eventListenters(){
-    const budgetForm = document.getElementById('budget-form');
-    const expenseForm = document.getElementById('expense-form');
-    const expenseList = document.getElementById('expense-list');
+  submitExpenseForm() {
+    const expenseInput = this.expenseInput.value;
+    const amountInput = this.amountInput.value;
     
-    const ui = new UI();
+    const self = this;
 
-    budgetForm.addEventListener('submit',function(event){
-              event.preventDefault();
-              
-              ui.submitBudgetForm();
-    })
+    if(expenseInput === ''){
+      this.expenseNameFeedback.classList.add('showItem');
+      this.expenseNameFeedback.innerHTML = `<p>your expense name can't be empty</p>`;
+        
 
-    expenseForm.addEventListener('submit',function(event){
-              event.preventDefault();
+      setTimeout(function(){
+          self.expenseNameFeedback.classList.remove("showItem"); 
+      },3000);
+    }
+ 
+    
+    if(amountInput === '' || amountInput < 0){
+      this.expenseValueFeedback.classList.add('showItem');
+      this.expenseValueFeedback.innerHTML = `<p>your expense value can't be empty or negative</p>`;
+      
+      setTimeout(function(){
+         self.expenseValueFeedback.classList.remove('showItem'); 
+      },3000);
+    }
 
-    })
-
-    expenseList.addEventListener('submit',function(event){
-              event.preventDefault();
-    })
+  }
 }
 
-document.addEventListener('DOMContentLoaded',function(){
-    eventListenters();
+function eventListenters() {
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+  const ui = new UI();
+
+  budgetForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    ui.submitBudgetForm();
+  })
+
+  expenseForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    ui.submitExpenseForm();
+  })
+
+  expenseList.addEventListener('submit', function (event) {
+    event.preventDefault();
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  eventListenters();
 })
